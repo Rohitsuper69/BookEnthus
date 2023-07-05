4553a9af85ccef09c4cb7084cd8a9683c0612167
Rohitsuper69 <rohitu.chakraborty.che21@itbhu.ac.in>
2023-07-05 10:42:53 +0530

    Change

--- mainapp/settings.py
+++ mainapp/settings.py
@@ -138,3 +138,7 @@ CRISPY_TEMPLATE_PACK="bootstrap5"
 
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_URL = '/static/'
