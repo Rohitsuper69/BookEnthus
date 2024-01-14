559ea6f2165a22a4f936760d80ea42dd800cc5b7
Rohitsuper69 <rohitu.chakraborty.che21@itbhu.ac.in>
2024-01-14 19:38:08 +0530

    Ss_added

--- mainapp/settings.py
+++ mainapp/settings.py
@@ -85,9 +85,9 @@ DATABASES = {
     }
 }
 
-DATABASES={
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
+# DATABASES={
+#     "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+# }
 
 
 # Password validation
